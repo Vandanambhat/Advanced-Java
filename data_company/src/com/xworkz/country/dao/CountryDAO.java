ca96b4bb98d71dfeaeef53fa2372421617ad2fd1
Vandanambhat <mvandana.xworkz@gmail.com>
2022-01-10 14:55:35 +0530

    hibernate

---
+++ data_company/src/com/xworkz/country/dao/CountryDAO.java
@@ -0,0 +1,8 @@
+package com.xworkz.country.dao;
+
+import com.xworkz.country.entity.CountryEntity;
+
+public interface CountryDAO {
+
+	public void create(CountryEntity entity);
+}
---
+++ data_company/src/com/xworkz/country/dao/CountryDAOimpl.java
@@ -0,0 +1,33 @@
+package com.xworkz.country.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+import com.xworkz.country.entity.CountryEntity;
+
+public class CountryDAOimpl implements CountryDAO{
+
+	@Override
+	public void create(CountryEntity entity) {
+		System.out.println("Invoked create in DAO");
+		System.out.println(entity);
+
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		configuration.addAnnotatedClass(CountryEntity.class);
+		SessionFactory factory = configuration.buildSessionFactory();
+		if (factory != null) {
+			Session session = factory.openSession();
+			Transaction tx = session.beginTransaction();
+			session.save(entity);
+			tx.commit();
+			session.close();
+		}
+		factory.close();
+		
+	}
+
+}
